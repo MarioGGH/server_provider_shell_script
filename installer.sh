d4abd94732ae9b1aa1a3cb25d51f0dc2960413dd
Mario Gibrain Hernández Hernández <119134359+Rotrys@users.noreply.github.com>
2025-04-04 18:33:26 +0000

    CHANGE
    change port 21 to 20

--- installer.sh
+++ installer.sh
@@ -11,14 +11,14 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 
 # Configurar vsftpd
-#echo "local_enable=YES" | sudo tee -a /etc/vsftpd.conf
-#echo "write_enable=YES" | sudo tee -a /etc/vsftpd.conf
-#echo "chroot_local_user=YES" | sudo tee -a /etc/vsftpd.conf
-#echo "allow_writable_chroot=NO" | sudo tee -a /etc/vsftpd.conf
+echo "local_enable=YES" | sudo tee -a /etc/vsftpd.conf
+echo "write_enable=YES" | sudo tee -a /etc/vsftpd.conf
+echo "chroot_local_user=YES" | sudo tee -a /etc/vsftpd.conf
+echo "allow_writable_chroot=NO" | sudo tee -a /etc/vsftpd.conf
 
 # Configurar el firewall (solo puertos necesarios)
 sudo ufw allow 80/tcp  # Permitir HTTP (puerto 80)
-sudo ufw allow 21/tcp  # Permitir FTP (puerto 21)
+sudo ufw allow 20/tcp  # Permitir FTP (puerto 21)
 
 # Recargar servicios
 sudo systemctl reload vsftpd
