32d87100baec54bbcab43965781e403c301789cb
Mario Gibrain Hernández Hernández <119134359+Rotrys@users.noreply.github.com>
2025-04-04 18:13:06 +0000

    CHANGES
    LITTLLE UPDATE

--- installer.sh
+++ installer.sh
@@ -11,10 +11,10 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 
 # Configurar vsftpd
-echo "local_enable=YES" | sudo tee -a /etc/vsftpd.conf
-echo "write_enable=YES" | sudo tee -a /etc/vsftpd.conf
-echo "chroot_local_user=YES" | sudo tee -a /etc/vsftpd.conf
-echo "allow_writable_chroot=NO" | sudo tee -a /etc/vsftpd.conf
+#echo "local_enable=YES" | sudo tee -a /etc/vsftpd.conf
+#echo "write_enable=YES" | sudo tee -a /etc/vsftpd.conf
+#echo "chroot_local_user=YES" | sudo tee -a /etc/vsftpd.conf
+#echo "allow_writable_chroot=NO" | sudo tee -a /etc/vsftpd.conf
 
 # Configurar el firewall (solo puertos necesarios)
 sudo ufw allow 80/tcp  # Permitir HTTP (puerto 80)
